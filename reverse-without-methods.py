2911395161f449bf8ba8aab59ba784788837ed46
Prajwal S <prajwalplays@gmail.com>
2024-01-27 01:14:05 +0530

    Create reverse-without-methods.py

---
+++ reverse-without-methods.py
@@ -0,0 +1,12 @@
+def reverse(num):
+    rev = 0
+    for _ in range(len(str(num))):
+        digit = num % 10            #mod wil return the remainder
+        rev = rev * 10 + digit      #will make it reverse
+        num = num // 10        #extract next_number by floor div rounds down the result to the nearest integer
+    
+    return rev  
+
+
+num = int(input("enter a number: "))
+print("reversed number is: ",reverse(num))
