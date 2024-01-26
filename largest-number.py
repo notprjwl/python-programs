a4c0c37513dcbb6932df554d5b8716480771b6a8
Prajwal S <prajwalplays@gmail.com>
2024-01-27 01:14:12 +0530

    Create largest-number.py

---
+++ largest-number.py
@@ -0,0 +1,17 @@
+#Write a Python program to find the largest element in a list.
+
+def find_largest(numbers):
+    
+    if not numbers:
+        return "empty list"
+    
+    largest = numbers[0]
+    
+    for num in numbers:
+        if(num > largest):
+            largest = num
+            
+    return largest
+
+numbers = [5, 12, 8, 3, 21, 17 ,91 ,82, 70000]
+print("The largest number is:", find_largest(numbers))       
