40e59e0dd0e8bca4228dc843dcf7f6649bb71fa3
Prajwal S <prajwalplays@gmail.com>
2024-01-27 19:39:31 +0530

    Update reverse-without-methods.py

--- reverse-without-methods.py
+++ reverse-without-methods.py
@@ -1,10 +1,13 @@
+from ast import If
+
+
 def reverse(num):
     rev = 0
     for _ in range(len(str(num))):
         digit = num % 10            #mod wil return the remainder
         rev = rev * 10 + digit      #will make it reverse
         num = num // 10        #extract next_number by floor div rounds down the result to the nearest integer
-    
+
     return rev  
 
 
