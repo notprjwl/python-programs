dd8e129b9ef1b421d0427f18215b414ec3ba85eb
Prajwal S <prajwalplays@gmail.com>
2024-01-29 22:20:14 +0530

    Create anagrams.py

---
+++ anagrams.py
@@ -0,0 +1,21 @@
+# An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+#egL SILENT -> LISTEN
+
+from regex import D
+
+
+def anagram(str1, str2):
+    count = 0
+    for i in str1:
+        for j in str2:
+            if i == j:
+                count += 1
+                
+    if count == len(str1):
+        return 'Strings are anagram'
+    else:
+        return "Strings are not anagram"
+    
+str1 = "silent"
+str2 = "listen"
+print(anagram(str1, str2))
