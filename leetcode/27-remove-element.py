48006f5f86a22ad9cefcb018b6f4b0852a99275d
Prajwal S <prajwalplays@gmail.com>
2024-05-10 22:40:34 +0530

    Create 27-remove-element.py

---
+++ leetcode/27-remove-element.py
@@ -0,0 +1,11 @@
+def removeElement(nums, val):
+    i = 0
+    while i < len(nums):
+        if nums[i] == val:
+            nums.pop(i)
+        else:
+            i += 1
+    return len(nums)
+
+print(removeElement([0,1,2,2,3,0,4,2], 2))
+
