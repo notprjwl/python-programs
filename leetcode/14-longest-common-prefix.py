b63e99ed6d4453ccde5299998e99c3067b3a2aec
Prajwal S <prajwalplays@gmail.com>
2024-10-12 15:56:49 +0530

    Update 14-longest-common-prefix.py

--- leetcode/14-longest-common-prefix.py
+++ leetcode/14-longest-common-prefix.py
@@ -1,16 +1,27 @@
-strs = ["flower","flow","flight"]
+strs = ["dog","racecar","car"]
 
-def longest_common_prefix(strs):
-    result = ""
-    for i in range(len(strs[0])):
-        for s in strs:
-            if i==len(s) or s[i] != strs[0][i]:
-                return result
-        result += strs[0][i]
+# def longest_common_prefix(strs):
+#     result = ""
+#     for i in range(len(strs[0])):
+#         for s in strs:
+#             if i==len(s) or s[i] != strs[0][i]:
+#                 return result
+#         result += strs[0][i]
             
                 
-        
-            
-    
+# print(longest_common_prefix(strs))
 
+
+def longest_common_prefix(strs):
+    prefix = strs[0]
+    for i in range(len(prefix)):
+        for j in range(len(strs)):
+            if i >= len(strs[j]) or strs[j][i] != prefix[i]: 
+                return prefix[:i]     
+    return prefix
+        
+        
 print(longest_common_prefix(strs))
+
+
+
